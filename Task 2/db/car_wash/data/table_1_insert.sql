958da9bc43eb5182543a19f7b6c4599e0c668e6c
AtanasKisyov <62992871+AtanasKisyov@users.noreply.github.com>
2022-07-12 22:54:12 +0300

    Delete Task 2/db directory

--- Task 2/db/car_wash/data/table_1_insert.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO car_washes (car_wash_id, car_wash_name, car_wash_address, quantity_of_places)
-VALUES (1, 'Car Wash At The End Of The Universe', 'At The End Of The Universe', 1),
-       (2, 'Car Wash At The Beginning Of The Universe', 'At The Beginning Of The Universe', 1),
-       (3, 'Car Wash At The Middle Of The Universe', 'At The Middle Of The Universe', 1);
--- Task 2/db/car_wash/schema/table_1_car_washes.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE car_washes (
-    car_wash_id INTEGER PRIMARY KEY,
-    car_wash_name VARCHAR(100) NOT NULL UNIQUE,
-    car_wash_address VARCHAR(100) NOT NULL,
-    quantity_of_places INTEGER NOT NULL
-);
