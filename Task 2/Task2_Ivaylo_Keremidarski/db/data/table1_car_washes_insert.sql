2052fb0f4fef1b8e2726cc904c0da4f4c1c1f3fb
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2022-07-13 16:07:34 +0300

    Add files via upload

---
+++ Task 2/Task2_Ivaylo_Keremidarski/db/data/table1_car_washes_insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO car_washes (car_wash_id, car_wash_name, car_wash_address, quantity_of_places)
+VALUES ('Ace Car Wash', '402 Kroger Way', 3),
+       ('Shinetastic', '3 Chelsea Dr', 3),
+       ('Quick Clean', '1521 E Alexis Rd', 1);
+       
---
+++ Task 2/Task2_Ivaylo_Keremidarski/db/data/table7_accounts_insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO accounts (user_id, email, password)
+VALUES (1, 'davidsmith@gmail.com', 'secret123'),
+       (2, 'garciam@gmail.com', 'losiento7'),
+       (3, 'doublej@gmail.com', 'qwerty77');
