f241b19f96d6b1356fb920e188dcf2d5e534b78c
SvetoslavKazandzhiev <80912240+SvetoslavKazandzhiev@users.noreply.github.com>
2022-07-13 11:09:04 +0100

    Add files via upload

---
+++ Task 2/Task2_Svetoslav_Kazandzhiev/db/carwash/data/insert_car_wash_places.sql
@@ -0,0 +1,5 @@
+INSERT INTO carwash_palces (carwash_place_id,
+			place_id,
+			carwash_id,
+			is_free)
+VALUES (1, 2, 1, True);
---
+++ Task 2/Task2_Svetoslav_Kazandzhiev/db/carwash/data/insert_carwash_services.sql
@@ -0,0 +1,3 @@
+INSERT INTO carwash_services (service_id,
+				carwash_id)
+VALUES (1, 1)
