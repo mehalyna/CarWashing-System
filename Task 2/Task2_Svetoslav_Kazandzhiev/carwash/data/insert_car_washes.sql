916753be655eeb5cb7582d8b77b5c975110be586
SvetoslavKazandzhiev <80912240+SvetoslavKazandzhiev@users.noreply.github.com>
2022-07-13 11:09:11 +0100

    Add files via upload

---
+++ Task 2/Task2_Svetoslav_Kazandzhiev/carwash/data/insert_car_washes.sql
@@ -0,0 +1,5 @@
+INSERT INTO Car_wash( car_wash_id,
+		    car_wash_name,
+		    car_wash_address,
+		    quantity_of_places)
+VALUES (1,'tata', 'sofia', 1);
---
+++ Task 2/Task2_Svetoslav_Kazandzhiev/carwash/data/insert_orders.sql
@@ -0,0 +1,8 @@
+INSERT INTO Orders (
+	order_id,
+	carwash_id,
+	user_id,
+	order_current_status,
+	order_date_time,
+	execution)
+VALUES (1, 2, 1, 'confirmed', 08:00, 15:00);
