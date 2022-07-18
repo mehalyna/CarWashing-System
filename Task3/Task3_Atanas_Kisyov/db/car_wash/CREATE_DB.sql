d66a94aea2b50c4513dcb01e7943d8a7ef5f9770
AtanasKisyov <atanaskisyov9@gmail.com>
2022-07-18 18:33:28 +0300

    update file names

--- Task3/Task3_Atanas_Kisyov/db/car_wash/CREATE_DB.sql
+++ Task3/Task3_Atanas_Kisyov/db/car_wash/CREATE_DB.sql
@@ -1,11 +1,11 @@
 \c postgres
 
-\i schema/table_1_car_wash.sql
-\i schema/table_2_places.sql
-\i schema/table_3_car_wash_places.sql
-\i schema/table_4_services.sql
-\i schema/table_5_car_wash_services.sql
-\i schema/table_6_users.sql
-\i schema/table_7_accounts.sql
-\i schema/table_8_orders.sql
-\i schema/table_9_order_details.sql
+\i schema/car_washes.sql
+\i schema/places.sql
+\i schema/car_wash_places.sql
+\i schema/services.sql
+\i schema/car_wash_services.sql
+\i schema/users.sql
+\i schema/accounts.sql
+\i schema/orders.sql
+\i schema/order_details.sql
--- Task3/Task3_Atanas_Kisyov/db/car_wash/INSERT_DATA.sql
+++ Task3/Task3_Atanas_Kisyov/db/car_wash/INSERT_DATA.sql
@@ -1,11 +1,11 @@
 
-\i data/table_1_insert.sql;
-\i data/table_2_insert.sql;
-\i data/table_3_insert.sql;
-\i data/table_4_insert.sql;
-\i data/table_5_insert.sql;
-\i data/table_6_insert.sql;
-\i data/table_7_insert.sql;
-\i data/table_8_insert.sql;
-\i data/table_9_insert.sql;
+\i data/car_washes.sql;
+\i data/places.sql;
+\i data/car_wash_places.sql;
+\i data/services.sql;
+\i data/car_wash_services.sql;
+\i data/users.sql;
+\i data/accounts.sql;
+\i data/orders.sql;
+\i data/order_details.sql;
 
