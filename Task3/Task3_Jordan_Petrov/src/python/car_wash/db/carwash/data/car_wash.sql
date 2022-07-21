6c0b8eb5667845e9ce650a07b9078b208792ccf3
Jordan Petrov <51748248+jordan5rov@users.noreply.github.com>
2022-07-21 18:20:06 +0300

    Add files via upload

---
+++ Task3/Task3_Jordan_Petrov/src/python/car_wash/db/carwash/data/car_wash.sql
@@ -0,0 +1,29 @@
+INSERT INTO carwashes (carwash_name, carwash_address, quantity_of_places)
+ VALUES ('Izio', '54 Northfield Drive', 40);
+
+INSERT INTO carwashes (carwash_name, carwash_address, quantity_of_places)
+ VALUES ('Omba', '4256 Thackeray Avenue', 1);
+
+INSERT INTO carwashes (carwash_name, carwash_address, quantity_of_places)
+ VALUES ('Twinder', '98369 Sachtjen Pass', 78);
+
+INSERT INTO carwashes (carwash_name, carwash_address, quantity_of_places)
+ VALUES ('Roombo', '8039 Memorial Place', 92);
+
+INSERT INTO carwashes (carwash_name, carwash_address, quantity_of_places)
+ VALUES ('Shufflester', '00 Arizona Point', 56);
+
+INSERT INTO carwashes (carwash_name, carwash_address, quantity_of_places)
+ VALUES ('Meevee', '65 Pleasure Plaza', 19);
+
+INSERT INTO carwashes (carwash_name, carwash_address, quantity_of_places)
+ VALUES ('Podcat', '4 Darwin Hill', 78);
+
+INSERT INTO carwashes (carwash_name, carwash_address, quantity_of_places)
+ VALUES ('Jayo', '094 Merrick Avenue', 56);
+
+INSERT INTO carwashes (carwash_name, carwash_address, quantity_of_places)
+ VALUES ('Twitterwire', '4873 Colorado Terrace', 39);
+ 
+INSERT INTO carwashes (carwash_name, carwash_address, quantity_of_places)
+ VALUES ('Gabcube', '410 Fuller Parkway', 33);
---
+++ Task3/Task3_Jordan_Petrov/src/python/car_wash/db/carwash/schema/accounts.sql
@@ -0,0 +1,6 @@
+CREATE TABLE accounts(
+    account_id SERIAL PRIMARY KEY NOT NULL,
+    user_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE,
+    email VARCHAR(50) NOT NULL, 
+    password VARCHAR(50) NOT NULL
+);
