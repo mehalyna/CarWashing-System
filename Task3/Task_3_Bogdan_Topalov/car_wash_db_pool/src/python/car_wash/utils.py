dc8402e4581e6f81d5dbd4f3e711ca1bab2a33e3
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-07-22 17:32:13 +0300

    Add scr

---
+++ Task3/Task_3_Bogdan_Topalov/car_wash_db_pool/src/python/car_wash/utils.py
@@ -0,0 +1,17 @@
+class Singleton(type):
+
+    """
+    Singleton class
+
+    Example of usage:
+
+    class Logger(metaclass=Singleton):
+        pass
+    """
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
