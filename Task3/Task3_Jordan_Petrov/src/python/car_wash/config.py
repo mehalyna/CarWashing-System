94cce7e9b3ab078b6e34510f1829626b90dcba65
Jordan Petrov <51748248+jordan5rov@users.noreply.github.com>
2022-07-23 13:31:30 +0300

    Add files via upload

--- Task3/Task3_Jordan_Petrov/src/python/car_wash/config.py
+++ Task3/Task3_Jordan_Petrov/src/python/car_wash/config.py
@@ -1,17 +1,38 @@
 """Fill this in constants with your db connection parameters."""
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--ro",
+    type=int,
+    help='Number of read only connections',
+    default=3
+)
+
+parser.add_argument(
+    "--rw",
+    type=int,
+    help='Number of read and write connections',
+    default=3
+)
+
+args = parser.parse_args()
+
+ro_num = args.ro
+rw_num = args.rw
 
 READ_ONLY_USER = {
-    'host': 'localhost',
-    'port': 5432,
-    'user': 'car_wash_user',
+    'hostname': 'localhost',
+    'username': 'car_wash_user',
     'password': 'user',
-    'dbname': 'carwashdb',
+    '_dbname': 'carwashdb',
+    'pool_size': ro_num,
 }
 
 READ_WRITE_USER = {
-    'host': 'localhost',
-    'port': 5432,
-    'user': 'postgres',
+    'hostname': 'localhost',
+    'username': 'postgres',
     'password': 'admin',
-    'dbname': 'carwashdb',
+    '_dbname': 'carwashdb',
+    'pool_size': rw_num,
 }
--- Task3/Task3_Jordan_Petrov/src/python/car_wash/main.py
+++ Task3/Task3_Jordan_Petrov/src/python/car_wash/main.py
@@ -1,14 +1,22 @@
-from src.python.car_wash.pool import read_only, read_write
+import logging
+
+from src.python.car_wash.dbpool import ro_db_pool, rw_db_pool
 
 
 def main():
+    logging.basicConfig(
+        filename='../../../pool.log',
+        format='%(asctime)s - %(message)s',
+        level=logging.INFO
+    )
+
     """Insert data into carwashes table"""
-    with read_write as cur:
+    with rw_db_pool.transaction() as cur:
         cur.execute('INSERT INTO carwashes (carwash_name, carwash_address, quantity_of_places) '
                     'VALUES (%s, %s, %s)', ('testing', 'testing', 20))
 
     """Print all the rows from carwashes table"""
-    with read_only as cur:
+    with ro_db_pool.connection() as cur:
         cur.execute('SELECT * FROM carwashes;')
         for row in cur.fetchall():
             print(row)
