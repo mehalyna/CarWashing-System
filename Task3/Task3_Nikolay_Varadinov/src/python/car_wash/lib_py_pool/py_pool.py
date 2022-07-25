4c1a6ce01d8529641258b540814c7540e9bba486
NikolayVaradinov <nikolayvaradinov@gmail.com>
2022-07-25 19:03:44 +0300

    _Connect try to pull once, if there is no connection it create new.

--- Task3/Task3_Nikolay_Varadinov/src/python/car_wash/lib_py_pool/py_pool.py
+++ Task3/Task3_Nikolay_Varadinov/src/python/car_wash/lib_py_pool/py_pool.py
@@ -1,5 +1,6 @@
 """Module with Read and Write Connection Pool."""
 
+
 import logging
 import time
 import typing
@@ -20,14 +21,6 @@ class PoolConn(metaclass=SingletonMeta):
 
     """Pool which is python list and strores DB connections."""
 
-    @staticmethod
-    def stringer(db_config: dict) -> str:
-        """Create string of key word arguments from dict."""
-
-        config = (" ").join(
-            f"{key}={value}" for key, value in db_config.items())
-        return config
-
     def __init__(self, size: int, *args, **kwargs) -> None:
         """Accept db_config as dict in args or as kwargs."""
 
@@ -35,10 +28,9 @@ class PoolConn(metaclass=SingletonMeta):
         self.pool = []
         self.size = size
         self._lock = Lock()
-        self._populate_pool()
 
     @property
-    def db_config(self):
+    def db_config(self) -> dict:
         """Nicer __init__ with this."""
 
         return self._db_config
@@ -48,49 +40,32 @@ class PoolConn(metaclass=SingletonMeta):
         """Checks if db_config is given as dict or kwargs."""
 
         if len(config_tuple[0]) != 0:
-            self._db_config = self.stringer(config_tuple[0][0])
+            self._db_config = config_tuple[0][0]
         else:
-            self._db_config = self.stringer(config_tuple[1])
+            self._db_config = config_tuple[1]
 
-    def _create_connection(self) -> psycopg2.connect:  # type: ignore
-        """Create database connection with psycopg2."""
+    def _connection(self) -> psycopg2.connect: #type: ignore
+        """Create database connection with psycopg2.
+        or take it from pool if there is one. """
 
-        return psycopg2.connect(self.db_config)
+        one_try = 0
+        conn = None
+        while not conn:
+            if len(self.pool) != 0:
+                with self._lock:
+                    conn = self.pool.pop()
+                    logging.info("PULL")
+                break
 
-    def _pull(self) -> psycopg2.connect:  # type: ignore
-        """Pull connection. Using Lock to prevent the race conditions."""
-
-        with self._lock:
-            while len(self.pool) == 0:
-                logging.info("No connection available. Wait a second.")
-                time.sleep(1)
-            conn = self.pool.pop()
-            logging.info("Connection PULLED")
-            return conn
-
-
-    def _push(self, conn) -> None:
-        """Push connection to pool."""
-
-        if len(self.pool) < self.size:
-            self.pool.append(conn)
-            logging.info("Connection PUSHED")
-        if len(self.pool) > self.size:
-            raise Exception("Pool is full. Should not have this connection!")
-
-    def _populate_pool(self) -> None:
-        """Create connections and add to pool."""
-
-        for _ in range(self.size):
-            try:
-                conn = self._create_connection()
+            if one_try:
+                conn = psycopg2.connect(**self._db_config)
                 logging.info("Create connection")
-                self._push(conn)
-            except psycopg2.Error:
-                print("DB Error")
-        logging.info("%s populated with %d connections",
-                     self.__class__.__name__, self.size)
-
+                break
+            one_try += 1
+        else:
+            logging.info("Wait a sec.")
+            time.sleep(1)
+        return conn
 
 class PoolReadConn(PoolConn):
 
@@ -100,25 +75,23 @@ class PoolReadConn(PoolConn):
     def transaction(self) -> typing.Generator:
         """Pull connection, and after use return it to pool"""
 
-
-        conn = self._pull()
         try:
+            conn = self._connection()
             curs = conn.cursor()
             yield curs
         except psycopg2.Error:
-            print(f"Problem with transaction {exc_info()}")
-        except IndexError:
-            print("Try again later.")
+            error = f"Problem with transaction {exc_info()}"
+            logging.info(error)
         else:
+            logging.info("READ")
             curs.close()
-            logging.info("Read from DB")
-        finally:
-            try:
-                conn.isolation_level
-            except psycopg2.OperationalError:
-                print("Connection dead")
-                conn = self._create_connection
-            self._push(conn)
+            if len(self.pool) < self.size:
+                self.pool.append(conn)
+                logging.info("PUSH")
+            else:
+                conn.close()
+                logging.info("Close connection")
+
 
 
 
@@ -130,22 +103,20 @@ class PoolWriteConn(PoolConn):
     def transaction(self) -> typing.Generator:
         """Pull connection, and after use return it to pool"""
 
-        conn = self._pull()
         try:
+            conn = self._connection()
             curs = conn.cursor()
             yield curs
         except psycopg2.Error:
-            print(f"Problem with transaction {exc_info()}")
-        except IndexError:
-            print("Try again later.")
+            error = f"Problem with transaction {exc_info()}"
+            logging.info(error)
         else:
+            logging.info("COMMIT")
             conn.commit()
             curs.close()
-            logging.info('Commit to DB')
-        finally:
-            try:
-                conn.isolation_level
-            except psycopg2.OperationalError:
-                print("Connection dead")
-                conn = self._create_connection
-            self._push(conn)
+            if len(self.pool) < self.size:
+                self.pool.append(conn)
+                logging.info("PUSH")
+            else:
+                conn.close()
+                logging.info("Close connection")
