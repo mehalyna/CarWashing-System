a392b271806e7a242f5b1f313a14cb74c29350e1
AtanasKisyov <atanaskisyov9@gmail.com>
2022-07-26 19:03:22 +0300

    remove comments

--- Task3/Task3_Atanas_Kisyov/src/python/car_wash/main.py
+++ Task3/Task3_Atanas_Kisyov/src/python/car_wash/main.py
@@ -1,5 +1,6 @@
 """Main file which executes the script"""
 import sys
+import logging
 
 from config import USER_CREDENTIALS
 from db_connection.connection_pools import ReadOnlyPool, ReadWritePool
@@ -8,7 +9,6 @@ from db_connection.connection_pools import ReadOnlyPool, ReadWritePool
 def main():
     """Interact with user"""
 
-    # Create connection pools
     read_only_pool = ReadOnlyPool(**USER_CREDENTIALS)
     read_write_pool = ReadWritePool(**USER_CREDENTIALS)
 
@@ -21,7 +21,7 @@ def main():
         cursor.execute('SELECT * FROM users')
         table = cursor.fetchall()
         for row in table:
-            print(row)
+            logging.info(row)
 
 
 if __name__ == '__main__':
