07b9b11f743aaf0cb6e282db8c02c4f77f058c41
NikolayVaradinov <70464482+NikolayVaradinov@users.noreply.github.com>
2022-07-27 17:08:41 +0300

    Delete db_pool

--- Task3/Task3_Nikolay_Varadinov/src/python/car_wash/dbpool.py
+++
@@ -1,8 +0,0 @@
-"""Dictionary with the configuration data for DB connection"""
-
-CAR_WASH_DB_CONFIG = {'host': 'localhost',
-                      'port': 5432,
-                      'user': 'postgres',
-                      'password': 'nik123',
-                      'dbname': 'postgres'
-                      }
