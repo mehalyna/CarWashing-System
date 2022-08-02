cb19b90e0eceb830ec6289c5aa18f618fd19d42e
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-08-02 20:10:37 +0300

    Update settings.py

--- Carwash/Carwash/settings.py
+++ Carwash/Carwash/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'car_washes',
     'order_details.apps.OrderDetailsConfig',
     'users',
 ]
