c5bd6cc05dd537f9f55253be773d403eaa232f09
AtanasKisyov <atanaskisyov9@gmail.com>
2022-08-03 22:12:12 +0300

    fix model

--- Carwash/orders/models.py
+++ Carwash/orders/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 from car_washes.models import CarWashes
-# from Carwash.users.models import Users
+from users.models import Users
 
 
 class Order(models.Model):
@@ -13,18 +13,19 @@ class Order(models.Model):
         on_delete=models.CASCADE,
         )
 
-    # user_id = models.ForeignKey(
-    #     User
-    # )
+    user_id = models.ForeignKey(
+        Users,
+        on_delete=models.CASCADE,
+    )
 
     order_current_status = models.CharField(
-        max_length=ORDER_CURRENT_STATUS_MAX_LENGTH
+        max_length=ORDER_CURRENT_STATUS_MAX_LENGTH,
     )
 
     order_date_time = models.DateTimeField(
-        auto_now_add=True
+        auto_now_add=True,
     )
 
     execution = models.DateTimeField(
-        auto_now=True
+        auto_now=True,
     )
