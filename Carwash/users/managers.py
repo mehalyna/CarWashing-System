fac7ba9bd1958a771a04616ff88eae27e72ef3c7
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-08-03 21:45:19 +0300

    Change boolean value

--- Carwash/users/managers.py
+++ Carwash/users/managers.py
@@ -16,7 +16,7 @@ class AccountsManager(BaseUserManager):
         return user
 
     def create_user(self, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
         return self._create_user(email, password, **extra_fields)
 
