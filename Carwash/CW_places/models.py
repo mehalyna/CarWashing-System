223d3c692f3ef44567a1bacd2b75c7fa36b82833
SvetoslavKazandzhiev <svetoslavkazandzhiev@gmail.com>
2022-08-04 19:48:16 +0300

    Adding CW_places app and model

---
+++ Carwash/CW_places/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class CWPlaces(models.Model):
+    carwash_place_id = models.IntegerField(primary_key=True, editable=False)
+    place_id = models.ForeignKey('Places', on_delete=models.CASCADE, null=True)
+    carwash_id = models.ForeignKey('Carwashes', on_delete=models.CASCADE, null=True)
+    is_free = models.BooleanField(default=False)
