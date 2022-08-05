e7804a5733777f638196a87e80364c0b0a25c96c
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-08-05 17:47:20 +0300

    Create UsersSerializer and add API view

--- Carwash/Carwash/urls.py
+++ Carwash/Carwash/urls.py
@@ -14,9 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework import routers
+
+from users.views import UserViewSet
+
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('car_washes.urls')),
+    path('', include('users.urls')),
 ]
