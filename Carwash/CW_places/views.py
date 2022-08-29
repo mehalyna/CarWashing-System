56a2231d4068f290c636efd942a0078724b4b7f1
SvetoslavKazandzhiev <svetoslavkazandzhiev@gmail.com>
2022-08-29 18:02:51 +0300

    Docstrings to CW_places app

--- Carwash/CW_places/views.py
+++ Carwash/CW_places/views.py
@@ -4,6 +4,7 @@ from CW_places.models import CWPlaces
 
 
 class CWPlacesView(viewsets.ModelViewSet):
+
+    """Python function that takes http requests and returns http response."""
     queryset = CWPlaces.objects.all()
     serializer_class = CWPlacesSerializer
-    permission = ()
