915370e82eccd36522606bed429ce8f66812d37c
NikolayVaradinov <70464482+NikolayVaradinov@users.noreply.github.com>
2022-08-29 12:18:12 +0300

    adding docstring to models

--- Carwash/order_details/models.py
+++ Carwash/order_details/models.py
@@ -1,3 +1,8 @@
+"""
+Application order_details has one table in postgresql,
+represented by OrderDetails Model.
+"""
+
 from django.db import models
 
 from orders.models import Order
@@ -6,6 +11,11 @@ from carwash_services.models import Services
 
 
 class OrderDetail(models.Model):
+    """Model for order_detail app. OrderDetails is in one to one dependancy
+    with Order model from orders app - 'OneToOneField'.
+    'order', 'service' and 'cawash_place' being fk's  are set to remove record
+    if coresponding records in the other models are removed - 'on_delete' cascade.
+    """
     order = models.OneToOneField(Order, on_delete=models.CASCADE)
     service = models.ForeignKey(Services, on_delete=models.CASCADE)
     carwash_place = models.ForeignKey(CWPlaces, on_delete=models.CASCADE)
@@ -14,6 +24,11 @@ class OrderDetail(models.Model):
     start_time = models.TimeField(blank=True, null=True)
 
     class Meta:
+        """Some data for the model.
+         With managed True, Django will create the appropriate database tables in migrate or as part 
+         of migrations and remove them as part of a flush management command.
+         order and service fields combination should be unique - 'unique_together'.
+        """
         managed = True
         db_table = 'order_detail'
         unique_together = (('service', 'order'),)
