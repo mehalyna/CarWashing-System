f3a52dc3b14bae27cf778e25be82fea5fbc9f305
NikolayVaradinov <nikolayvaradinov@gmail.com>
2022-08-29 11:43:23 +0300

    adding dockstrings

--- Carwash/order_details/urls.py
+++ Carwash/order_details/urls.py
@@ -1,3 +1,10 @@
+"""
+Set up entrypoints for order_details app.
+Use routers for simply instantiating a Router class, and then registering
+all the required ViewSets with that router.
+DefaultRouter adds API root view, and adds format suffix patterns to the URLs.
+"""
+
 from django.urls import include, path
 from rest_framework import routers
 from order_details.views import OrderDetailViewSet
