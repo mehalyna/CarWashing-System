1c8ad5026474bee38b57706528e04c55f5b9fd9d
AtanasKisyov <atanaskisyov9@gmail.com>
2022-08-30 21:09:14 +0300

    fix issue with returned data

--- Carwash/orders/serializers.py
+++ Carwash/orders/serializers.py
@@ -8,6 +8,22 @@ class OrderSerializer(serializers.ModelSerializer):
     Serializer for Order model.
     """
 
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize instance.
+        Check the request method and set the depth of related tables for JSON response.
+        """
+
+        super().__init__(*args, **kwargs)
+
+        request = self.context.get('request')
+
+        if request and request.method=='POST':
+            self.Meta.depth = 0
+        else:
+            self.Meta.depth = 2
+            self.Meta.read_only_fields = ('car_wash', 'user')
+
     class Meta:
         model = Order
         fields = '__all__'
--- Carwash/orders/views.py
+++ Carwash/orders/views.py
@@ -1,9 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
 
-from car_washes.models import CarWashes
 from orders.models import Order
 from orders.serializers import OrderSerializer
-from users.models import Accounts, Users
+from users.serializers import UsersSerializer
 
 
 class OrderView(ModelViewSet):
@@ -13,59 +12,7 @@ class OrderView(ModelViewSet):
     Attributes
         queryset: collection of all Order objects/rows in database
         serializer_class: reference to OrderSerializer
-
-    Methods
-
-        retrieve: self, request, *args, **kwargs
-            Override to alter returned 
-            information of foreign keys in JSON response
-
-    Private methods
-
-        _alter_user_information: self, data
-            returns the user information in a python dictionary
-
-        _alter_car_wash_information: self, data
-            returns the car_wash information in a python dictionary
     """
 
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-
-    def _alter_user_information(self, data: serializer_class) -> dict:
-        """Returns full user information"""
-
-        user_pk = data.data['user']
-        user = Users.objects.get(pk=user_pk)
-        account = Accounts.objects.get(pk=user.account.id)
-
-        return {
-            'account_id': account.id,
-            'full_name': user.full_name,
-            'phone_number': user.phone_number,
-            'user_location': user.user_location,
-            'email': account.email,
-        }
-
-    def _alter_car_wash_information(self, data: serializer_class) -> dict:
-        """Returns full car wash information"""
-
-        car_wash_pk = data.data['car_wash']
-        car_wash = CarWashes.objects.get(pk=car_wash_pk)
-
-        return {
-            'car_wash_id': car_wash.id,
-            'car_wash_name': car_wash.car_wash_name,
-            'car_wash_address': car_wash.car_wash_address,
-            'quantity_of_places': car_wash.quantity_of_places,
-        }
-
-    def retrieve(self, request, *args: tuple, **kwargs: dict) -> serializer_class:
-        """
-        Re-write default retrieve method to alter the returned data
-        """
-
-        data = super().retrieve(request, *args, **kwargs)
-        data.data['user'] = self._alter_user_information(data)
-        data.data['car_wash'] = self._alter_car_wash_information(data)
-        return data
