dfb6e68670d6253b0eb3b79f9816dc1f044b5db6
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-08-31 16:24:46 +0300

    Add custom actions for Accounts admin panel

--- Carwash/users/admin.py
+++ Carwash/users/admin.py
@@ -1,6 +1,29 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 
 from users.models import Users, Accounts
+
+
+def show_message(model, request, message):
+    return model.message_user(request, message, messages.SUCCESS)
+
+
+@admin.action(description='Make selected accounts superusers')
+def make_superuser(model, request, queryset):
+    queryset.update(is_superuser=True)
+    show_message(model, request, 'Superuser status was successfully changed.')
+
+
+@admin.action(description='Make selected accounts staff')
+def make_staff(model, request, queryset):
+    queryset.update(is_staff=True)
+    show_message(model, request, 'Staff status was successfully changed.')
+
+
+@admin.action(description='Make selected accounts regular')
+def make_regular(model, request, queryset):
+    queryset.update(is_staff=False)
+    queryset.update(is_superuser=False)
+    show_message(model, request, 'Superuser and staff statuses were successfully changed.')
 
 
 @admin.register(Accounts)
@@ -10,11 +33,13 @@ class AccountsAdmin(admin.ModelAdmin):
         - accounts can be searched by their email.
         - accounts emails and statuses are listed.
         - accounts can be filtered by their status.
+        - selected accounts statuses can be changed via custom actions.
     """
 
     search_fields = ('email__contains', )
     list_display = ('email', 'is_superuser', 'is_staff', 'is_active')
     list_filter = ('is_superuser', 'is_staff')
+    actions = [make_superuser, make_staff, make_regular]
 
 
 @admin.register(Users)
