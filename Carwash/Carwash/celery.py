3eb9c57a445fc5792b27e37c4bd4a9ffcc226fdb
jordan5rov <jordan5rov@gmail.com>
2022-09-02 18:51:46 +0300

    initialized flower

--- Carwash/Carwash/celery.py
+++ Carwash/Carwash/celery.py
@@ -5,7 +5,7 @@ from celery import Celery
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Carwash.settings')
 
-app = Celery('car_wash', backend='redis', broker='redis://localhost:6379')
+app = Celery()
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
