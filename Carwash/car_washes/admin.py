b495105f2acd70b224d3aef032cc838fdccd2c65
jordan5rov <jordan5rov@gmail.com>
2022-09-03 16:21:06 +0300

    admin action as task

--- Carwash/car_washes/admin.py
+++ Carwash/car_washes/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 
 from car_washes.models import CarWashes
+from car_washes.tasks import car_wash_task
 
-admin.site.register(CarWashes)
+# Connect custom action and celery task
+
+class CarWashAdmin(admin.ModelAdmin):
+    model = CarWashes
+    list_display = ('id', 'car_wash_name', 'car_wash_address', 'quantity_of_places')
+    list_display_links = ('id', 'car_wash_name')
+    search_fields = ('car_wash_name',)
+    list_per_page = 25
+    actions = ('car_wash_task',)
+
+    # def make_quantity_0(self, request, queryset):
+    #     queryset.update(quantity_of_places=0)
+
+
+admin.site.register(CarWashes, CarWashAdmin)
--- Carwash/car_washes/tasks.py
+++ Carwash/car_washes/tasks.py
@@ -1,7 +1,21 @@
 from __future__ import absolute_import
+
+from django.test.client import RequestFactory
+from django.contrib import admin
+
 from celery import shared_task
+
+from car_washes.models import CarWashes
 
 
 @shared_task
-def test(param):
-    return 'The tasks executed with the following parameter: "%s" ' % param
+def car_wash_task(pk_of_model):
+    queryset = CarWashes.objects.filter(pk=pk_of_model)
+
+    rf = RequestFactory()
+
+    request = rf.put('/car_washes/{}/'.format(pk_of_model)
+                     , data={'quantity_of_places': 0}
+                     , content_type='application/json')
+
+    admin.site._registry[CarWashes].change_view(request, pk_of_model, data={'quantity_of_places': 0})
