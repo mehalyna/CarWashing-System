3322f9130b4064fd4351c17b468a09a6a3cbe931
jordan5rov <jordan5rov@gmail.com>
2022-09-03 19:21:41 +0300

    changed tasks and actions

--- Carwash/car_washes/admin.py
+++ Carwash/car_washes/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 
 from car_washes.models import CarWashes
-from car_washes.tasks import car_wash_task
+from car_washes.tasks import set_car_wash_quantity_to_0
 
-# Connect custom action and celery task
 
 class CarWashAdmin(admin.ModelAdmin):
     model = CarWashes
@@ -11,10 +10,11 @@ class CarWashAdmin(admin.ModelAdmin):
     list_display_links = ('id', 'car_wash_name')
     search_fields = ('car_wash_name',)
     list_per_page = 25
-    actions = ('car_wash_task',)
+    actions = ('make_quantity_0',)
 
-    # def make_quantity_0(self, request, queryset):
-    #     queryset.update(quantity_of_places=0)
+    def make_quantity_0(self, request, queryset):
+        for car_wash in queryset:
+            set_car_wash_quantity_to_0.delay(car_wash.id)
 
 
 admin.site.register(CarWashes, CarWashAdmin)
